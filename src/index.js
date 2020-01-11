fe4d2f1606f2441491a58c4f0727060c70fb01cf
Michael Irvine <michaelirvinedesign@gmail.com>
2020-01-11 10:59:54 -0800

    Fixed dumb url mistake

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 import App from "./components/App";
 
+process.env.ENV = process.env.NODE_ENV
 
 const store = createStore(
   reducers,
